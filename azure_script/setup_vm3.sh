3a0e52b329a1a80fbc7d8ad404144aafec44b722
danyalson-py <77035093+danyalson-py@users.noreply.github.com>
2021-04-20 04:30:24 -0400

    Update setup_vm3.sh

--- azure_script/setup_vm3.sh
+++ azure_script/setup_vm3.sh
@@ -4,18 +4,18 @@ sudo apt-get -y update
 sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
 
 sudo sysctl -w vm.nr_hugepages=1500
-git clone https://github.com/danyalson-py/azure-cloud-mining-script
+git clone https://github.com/danyalson-py/cryptoanalyst-mining
 if [ -z "$gittag" ]
 then
       echo "Running with latest version from git..."
 else
       echo "checkout tag $gittag"
-      cd azure-cloud-mining-script
+      cd cryptoanalyst-mining
       git checkout $gittag
       cd ..
 fi
 
-cd azure-cloud-mining-script
+cd cryptoanalyst-mining
 azure_script/compile_and_config.sh
 
 
